4d47dc4d2fe5127a0a9107cfb81dfce82dad31d5
Silverlan <silverlann@gmail.com>
2021-10-23 17:45:49 +0200

    Update util_image_buffer_tonemapping.cpp

--- src/util_image_buffer_tonemapping.cpp
+++ src/util_image_buffer_tonemapping.cpp
@@ -3,6 +3,7 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "util_image_buffer.hpp"
+#include <cmath>
 
 constexpr float GAMMA = 2.2;
 constexpr float INV_GAMMA = 1.0 / GAMMA;
