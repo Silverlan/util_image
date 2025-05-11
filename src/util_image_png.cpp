3a13454dc3d74df48801ad15531704c80f58f28c
Silverlan <silverlann@gmail.com>
2025-05-11 19:56:39 +0200

    fix: build error

--- src/util_image_png.cpp
+++ src/util_image_png.cpp
@@ -51,7 +51,6 @@ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <cstring>
 
 #include <png.h>        /* libpng header */
-#include <pngstruct.h>
 
 // Header data
 #ifndef TRUE
